f8357e2c6a5f7f9aae134feb6b183ac17b2ad58d
whoisluci <lucmov99@gmail.com>
2024-12-09 18:56:49 +0100

    Small update for "pick your character"

--- components/characterPage/characterPage.js
+++ components/characterPage/characterPage.js
@@ -1,28 +1,32 @@
 import { createButton } from "../buttons/buttons.js";
 import { STATE } from "../index.js";
+import { updateChar } from "../waitingRoom/waitingRoom.js";
+import { renderWaitingRoom } from "../waitingRoom/waitingRoom.js";
 
 export function renderCharacterPage (parentID) {
     document.getElementById(parentID).innerHTML = "";
-    
-    /* If-sats som kollar vem som är inloggad spelaren */
-    // for (let player of STATE.room.players) {
-    //     if (player[id] === STATE.clientID) {
 
-    //     }
-    // }
+    /* När korten är på plats och det läggs eventListener på dem, så ska man lagra karaktären som spelaren har valt */
 
-    const bttn = createButton(parentID, "Play", "#FF5252", "190px");
+    const bttn = createButton(parentID, "Confirm", "#FF5252", "190px");
     bttn.addEventListener("click", () => {
 
         /* Här hämtar man karaktären som spelaren har valt */
             
         const data = {
-            event: "selectChar",
+            event: "pickChar",
             data: {
                 "clientID": STATE.clientID,
                 /* "selectedChar": här skickar man karaktären till servern */ 
             }
         };
-        // STATE.socket.send(JSON.stringify(data));
+
+        STATE.socket.send(JSON.stringify(data));
+
+        if (STATE.room.players.length === 2) {
+            /* updateChar(karaktären (objekt) man har valt) */
+        }
+
+        renderWaitingRoom("wrapper");
     });
 }
