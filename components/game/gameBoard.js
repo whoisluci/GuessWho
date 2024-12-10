d705a1db14f86348f8152fb480a14f9ed7f29e6f
whoisluci <lucmov99@gmail.com>
2024-12-10 13:52:48 +0100

    Small fixes

--- components/game/gameBoard.js
+++ components/game/gameBoard.js
@@ -1,7 +1,7 @@
 import { STATE } from "../index.js";
 
-export function renderGameBoard(parentID) {
+export function renderGameBoard(parentID, cards) {
     document.getElementById(parentID).innerHTML = "";
 
-    
+
 }
