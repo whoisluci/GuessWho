3e3aad2458ff10031c592494dfa98767b28474ba
hildahammer <hilda_hammer@yahoo.se>
2024-12-17 10:37:03 +0100

    designed arrow and helpBtn

--- components/header/header.js
+++ components/header/header.js
@@ -6,18 +6,18 @@ export function header (parentID) {
     container.id = "headerContainer";
     document.getElementById(parentID).append(container);
 
-    // const arrow = document.createElement('img');
-    // arrow.src = './media/ep_back.png';
-    // arrow.id = "arrowBack";
-    // arrow.style.width = "96px"
-    // arrow.style.height = "75px";
-    // container.append(arrow); 
+    const arrow = document.createElement('img');
+    arrow.src = '../../static/media/backArrow.png';
+    arrow.id = "arrowBack";
+    arrow.style.width = "45px"
+    arrow.style.height = "40px";
+    container.append(arrow); 
 
-    const arrowBack = document.createElement("span");
-    arrowBack.id = "arrowBack";
-    //arrowBack.innerHTML = `&#8592`;
-    arrowBack.innerHTML = "←";
-    container.append(arrowBack);
+    // const arrowBack = document.createElement("span");
+    // arrowBack.id = "arrowBack";
+    // //arrowBack.innerHTML = `&#8592`;
+    // arrowBack.innerHTML = "←";
+    // container.append(arrowBack);
 
     const logo = document.createElement('img');
     logo.src = '../static/media/logga.png';
@@ -26,9 +26,16 @@ export function header (parentID) {
     logo.style.height = "75px";
     container.append(logo); 
 
-    const helpBttn = createButton("wrapper", "?", "#FF5252", "35px");
+    // const helpBttn = createButton("wrapper", "?", "#FF5252", "35px");
+    // helpBttn.id = "helpBttnHeader";
+    // container.append(helpBttn);
+
+    const helpBttn = document.createElement('img');
+    helpBttn.src = '../static/media/infoBtn.png';
     helpBttn.id = "helpBttnHeader";
-    container.append(helpBttn);
+    helpBttn.style.width = "45px"; // Bredd på knappen
+    helpBttn.style.height = "45px";
+    container.append(helpBttn); // Lägger till elementet i containern
 
     helpBttn.addEventListener("click", () => { 
         renderPopupHowTo("Wrapper");
